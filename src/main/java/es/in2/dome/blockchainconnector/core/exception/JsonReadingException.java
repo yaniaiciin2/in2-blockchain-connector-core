cec5c62d5ebe27e22a14551cbab8564301350e57
oriolcd <oriol.canades@in2.es>
2023-09-05 18:39:42 +0200

    Refactor and fixes :)

--- src/main/java/es/in2/dome/blockchainconnector/core/exception/JsonReadingException.java
+++
@@ -1,9 +0,0 @@
-package es.in2.dome.blockchainconnector.core.exception;
-
-public class JsonReadingException extends RuntimeException {
-
-    public JsonReadingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
