a64401ef258373d8e8b0883a7e5d87e7b2b47665
Stefan Wiedemann <wistefan@googlemail.com>
2023-10-27 13:49:53 +0200

    doc

--- src/main/java/es/in2/blockchain/connector/integration/blockchainnode/configuration/BlockchainProperties.java
+++ src/main/java/es/in2/blockchain/connector/integration/blockchainnode/configuration/BlockchainProperties.java
@@ -4,6 +4,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
+/**
+ * Configuration to connect the blockchain
+ * @param rpcAddress - rpc address of the node to be used
+ * @param userEthereumAddress - address of the user in the ethereum compatible blockchain
+ * @param subscription - configuration to be used for subscribing at blockchain events
+ */
 @ConfigurationProperties(prefix = "blockchain")
 public record BlockchainProperties(String rpcAddress, String userEthereumAddress,
 								   @NestedConfigurationProperty BlockchainSubscriptionConfiguration subscription) {
--- src/main/java/es/in2/blockchain/connector/integration/blockchainnode/configuration/BlockchainSubscriptionConfiguration.java
+++ src/main/java/es/in2/blockchain/connector/integration/blockchainnode/configuration/BlockchainSubscriptionConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Configuration to be used for subscribing at the blockchain
+ * @param notificationEndpoint - endpoint to notify on events
+ * @param active - should the subscription be active
+ * @param eventTypes - type of events to subscribe to
+ */
 public record BlockchainSubscriptionConfiguration(String notificationEndpoint, boolean active,
 												  List<String> eventTypes) {
 
